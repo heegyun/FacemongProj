3fe7e92c9ab96dde3ee605329704235ab37887db
heegyun <yeom#dju.kr>
2021-08-12 22:19:00 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react'
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+const customers = [{
+  'id' : 1, 
+  'image' :'https://placeimg.com/64/64/1' ,
+  'name' : '푸들',
+  'birthday' : '961212',
+  'gender' : '남',
+  'age' : '2'
+},
+{
+  'id' : 2, 
+  'image' :'https://placeimg.com/64/64/2' ,
+  'name' : '코카스파니엘',
+  'birthday' : '960305',
+  'gender' : '여',
+  'age' : '3'
+},
+{
+  'id' : 3, 
+  'image' :'https://placeimg.com/64/64/3' ,
+  'name' : '치와와',
+  'birthday' : '921205',
+  'gender' : '남',
+  'age' : '5'
+
+}
+
+]
 
 class App extends Component {
   render(){
   return (
-    <div className="gray-background">
-      <img src={logo} lat='logo'/>
-      <h2>FaceMong App management system v.1</h2>
+    <div>
+     { 
+      customers.map(c =>{
+        return(
+          <Customer 
+            key = {c.id}
+            id= {c.id}
+            image = {c.image}
+            name = {c.name}
+            birhday = {c.birthday}
+            gender = {c.gender}
+            age = {c.age}
+            />
+        );
+      })
+     }
     </div>
    );
   }
